9d1374c1f5d0a225a6ce4d6d1c80e0b5e65d3136
Deia Karunyk <deia.karunyk@rubyplay.com>
2024-06-27 16:24:11 +0300

    Implemented reverse counter

--- hw1/script1.sh
+++ hw1/script1.sh
@@ -3,3 +3,9 @@ echo "HW 1 Task 1: Script timer"
 read -p "How can I call you? " username
 echo "Good day $username! Enter the integer please (0-9)"
 read inputvalue
+while [ "$inputvalue" -gt "0" ]; do
+    echo "$inputvalue"
+    inputvalue=$((inputvalue - 1))
+    sleep 1
+done
+read -p "Done!"
