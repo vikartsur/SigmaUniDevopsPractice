16e7e7684ccf66c50355156494b21c09bdccb395
Deia Karunyk <deia.karunyk@rubyplay.com>
2024-07-22 13:26:13 +0300

    Updated with checking input value

--- hw1/script1.sh
+++ hw1/script1.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 echo "HW 1 Task 1: Script timer"
 read -p "How can I call you? " username
-echo "Good day $username! Enter the integer please (0-9)"
-read inputvalue
-while [ "$inputvalue" -gt "0" ]; do
-    echo "$inputvalue"
-    inputvalue=$((inputvalue - 1))
-    sleep 1
+echo "Good day $username!"
+while true; do
+    echo "Enter the integer please: "
+    read inputvalue
+    if [[ $inputvalue =~ ^[0-9]+$ ]]; then
+        while [ $inputvalue != 0 ]; do
+            echo "$inputvalue"
+            inputvalue=$((inputvalue - 1))
+            sleep 1
+        done
+    else
+        echo "Enter the number. Chao bambino!"
+        break
+    fi
 done
 read -p "Done!"
